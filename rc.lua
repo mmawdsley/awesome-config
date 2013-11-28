13053ab73ae437ae6349ff2127a3ede6355c2cad
Matthew Mawdsley <mmawdsley@blueyonder.co.uk>
2013-11-28 19:45:56 +0000

    Running volume_notification in the background so it doesn't lock awesome.

--- rc.lua
+++ rc.lua
@@ -1081,7 +1081,7 @@ config.run.startup = {
   "update-notifier &",
   "xset s off",
   "xset -dpms",
-  "volume_notification awesome",
+  "volume_notification awesome &",
 }
 
 if hostname == "daedalus" then
@@ -1094,7 +1094,7 @@ config.run.restart = {
   "xset s off",
   "xset -dpms",
   "nitrogen --restore",
-  "volume_notification awesome",
+  "volume_notification awesome &",
 }
 
 config.startup = function ()
