32901d418dd2e84949c28efc6ac636836547d160
Matthew Mawdsley <matthew@mawdsley.me>
2020-12-26 16:46:35 +0000

    Add RSS badge

---
+++ scripts/rssbadge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from mailconfig import RSS_Config
+from totalmailboxes import TotalMailboxes
+from unseenbadge import UnseenBadge
+
+if __name__ == "__main__":
+    config = RSS_Config()
+    print(config)
+    total_mailboxes = TotalMailboxes(config, include_read=True)
+    UnseenBadge(total_mailboxes, "openrss.desktop").start()
